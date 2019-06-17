485b5c426659add31d2d9859d5c796af2ef5fec7
Eliran Darshan <eliran.darshan@live.biu.ac.il>
2019-06-17 13:24:25 +0300

    updates

--- app/src/main/java/com/example/ex4mobile/TcpClient.java
+++ app/src/main/java/com/example/ex4mobile/TcpClient.java
@@ -50,13 +50,6 @@ public class TcpClient {
         thread.start();
     }
 
-    public boolean sock() {
-        if (this.socket == null)
-            return false;
-        return true;
-    }
-
-
     public void sendValues(final String command) {
         try {
             System.out.println(command);
